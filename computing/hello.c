bbf801da8ca5cd1f53f1c4780db4527a3190e117
argus <tjdals4047@gmail.com>
2023-02-03 13:43:08 +0900

    Set up C/C++ env

---
+++ computing/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello1 hello.C!\n");
+    printf("Hello2 hello.C!\n");
+    printf("Hello3 hello.C!\n");
+    return 0;
+}
---
+++ computing/helloC++.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    cout<<"Hello1 C++"<<endl;
+    cout<<"Hello2 C++"<<endl;
+    cout<<"Hello3 C++"<<endl;
+    return 0;
+}
