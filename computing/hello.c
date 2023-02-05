422bbe606622d9d7539f9aac7e7f67cbdecbedee
argus <tjdals4047@gmail.com>
2023-02-05 09:20:16 +0900

    C lecture 1

--- computing/hello.c
+++ computing/hello.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#define MY_CONSTANT 10
 
-int main(){
-    printf("Hello1 hello.C!\n");
-    printf("Hello2 hello.C!\n");
-    printf("Hello3 hello.C!\n");
+int main(void){
+
+    printf("You defined a fixed value %d\n", MY_CONSTANT);
+    printf("Hello C!\n");
+
+    printf("Good bye C!\n");
     return 0;
 }
